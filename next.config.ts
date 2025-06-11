a395dadd941db70068f5d0ed9ec0542f985bd6dc
user.name like: "zeeshan <user.email like: "zeeshan484anwar@gmail.com>
2025-06-11 20:15:11 +0500

    fixed error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Good practice for catching potential issues
   images: {
     domains: ["media2.dev.to"],
   },
   experimental: {
-    // If you're using Next.js App Router (which you likely are), you can enable this
-    serverActions: true,
+    serverActions: {}, // ✅ instead of true
   },
-};
+}
 
-module.exports = nextConfig;
-
-
+module.exports = nextConfig
